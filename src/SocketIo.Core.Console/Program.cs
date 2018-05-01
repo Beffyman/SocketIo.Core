78ef141b70ca30ca2d02c7e7a75b6f6bbfe77d20
Juan Luis Guerrero <jlguerrero@gmail.com>
2018-05-01 08:39:24 +0200

    Update to NetCore 2.1

---
+++ src/SocketIo.Core.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+namespace SocketIo.Core.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var manager = new SocketIOManager("127.0.0.1", 3000);
+            manager.Run();
+
+            WriteLine();
+            WriteLine("Pulse INTRO para finalizar...");
+            ReadLine();
+        }
+    }
+}
---
+++ src/SocketIo.Core.Console/SocketIOManager.cs
@@ -0,0 +1,55 @@
+﻿//
+// https://github.com/Beffyman/SocketIo.Core
+//
+using Newtonsoft.Json;
+using SocketIo;
+using SocketIo.SocketTypes;
+using System;
+using static System.Console;
+
+namespace SocketIo.Core.Console
+{
+    public class SocketIOManager
+    {
+        private string _ip;
+        private ushort _port;
+
+        public SocketIOManager(string ip, ushort port)
+        {
+            _ip = ip;
+            _port = port;
+        }
+
+        public void Run()
+        {
+            var socket = Io.Create(_ip, _port, _port, SocketHandlerType.Udp);
+
+            socket.On("connect", () =>
+            {
+                WriteLine("Connected !");
+                socket.On("login", (string user) =>
+                {
+                    WriteLine("Logged !");
+                });
+
+
+                var login = new { name = "juanlu", password = "123456" };
+
+                var json = JsonConvert.SerializeObject(login);
+
+                socket.Emit("login",  json);
+            });
+
+            socket.Emit("connect");
+
+
+            WriteLine("Waiting for connection...");
+            WriteLine("Pulse INTRO para finalizar...");
+            ReadLine();
+
+            socket.Close();
+
+        }
+
+    }
+}
